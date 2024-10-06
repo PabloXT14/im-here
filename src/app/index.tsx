6ea484f0a6a9a4bf4d37fdf1f63bfc24f8422b5c
Pablo Alan <pabloxt14@gmail.com>
2024-10-05 22:41:02 -0300

    docs: styling elements

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,42 +1,32 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <StatusBar translucent backgroundColor="#000000" />
-      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
-
-      <View style={styles.square}>
-        <Text style={styles.squareText}>+</Text>
-      </View>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#131016',
+        padding: 24,
+      }}
+    >
+      <Text
+        style={{
+          color: '#fafafa',
+          fontSize: 24,
+          fontWeight: 'bold',
+          marginTop: 48,
+        }}
+      >
+        Nome do evento
+      </Text>
+      <Text
+        style={{
+          color: '#6b6b6b',
+          fontSize: 16,
+        }}
+      >
+        Sexta, 4 de Novembro de 2024
+      </Text>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fafafa',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#09090b',
-  },
-  square: {
-    width: 64,
-    height: 64,
-    backgroundColor: '#10b981',
-    padding: 8,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 8,
-  },
-  squareText: {
-    fontSize: 24,
-    color: '#fafafa',
-  },
-})
